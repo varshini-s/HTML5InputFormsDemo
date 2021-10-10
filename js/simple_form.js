8d99047e16007bc3bf431af873a57077e19a9fa1
varshini <dreamvarshini@gmail.com>
2021-10-10 16:13:56 +0530

    ADDED: defined eventlistener to check for invalid username

--- js/simple_form.js
+++ js/simple_form.js
@@ -4,3 +4,16 @@ const NAME_PATTERN = RegExp('^[A-Z]{1}[a-z]{2,}$');
 const PHONE_NUMBER_PATTERN = RegExp("^[0-9]{2}[ ][0-9]{10}$");
 const PASSWORD_PATERN = RegExp("^(?=.*[0-9])(?=.*[A-Z])(?=.{8,}$)[0-9a-zA-Z]*[@#$%][0-9a-zA-Z]*$");
 
+const text = document.querySelector('#text');
+const textError = document.querySelector('.text-error');
+text.addEventListener('input', function () {
+    if (NAME_PATTERN.test(text.value)) 
+    {
+        textError.textContent = "";
+    }
+    else 
+    {
+        textError.textContent = "Name is Incorrect";
+    }
+});
+
